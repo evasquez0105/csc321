5a8dc18e338653bca6c8c3e7429fef0b0c535842
EV <evasquez105@toromail.csudh.edu>
2026-02-25 01:53:09 +0000

    Lab #7

---
+++ evasquez7.py
@@ -0,0 +1,25 @@
+#Elmer Vasquez
+#Lab #7
+
+x = 3
+y = 2.7
+z = "Los Angeles"
+
+if x > 4:
+    print("x is greater than 4")
+elif x < 4:
+    print("x is less than 4")
+else:
+    print("x is ", x, " which is equal to 4")
+
+if y > 6.7:
+    print("y is greater than 6.7")
+elif y < 6.7:
+    print("y is less than 6.7")
+else:
+    print("y is ", y, " which is equal to 6.7")
+
+if z == "Los Angeles":
+    print("z does equal to Los Angeles")
+else:
+    print("z does not equal to Los Angeles")
